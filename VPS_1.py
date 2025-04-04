778a3a5110b9a58ded66a6b01ea7ba7324983e98
dev_gyuil <a46884334@gmail.com>
2025-04-04 18:13:22 +0900

    day2

---
+++ VPS_1.py
@@ -0,0 +1,21 @@
+data = int(input())
+
+for i in range(data):
+    list = []
+    data2 = input()
+
+    for j in data2:
+        if j == '(':
+            list.append(j)
+        elif j == ')':
+            if list:
+                list.pop()
+            else :
+                print("NO")
+                break
+
+    else :
+        if not list:
+            print("YES")
+        else :
+            print("NO")
