a3b3f62fef1ae9f53ea36f89ccfe98af126656a4
dev_gyuil <a46884334@gmail.com>
2025-04-14 17:10:02 +0900

    day 7

---
+++ 10808_알파벳_개수.py
@@ -0,0 +1,7 @@
+n = list(str(input()))
+         
+alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+for i in alphabets:
+	y = n.count(i)
+	print(y, end=' ')
