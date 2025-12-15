7edb12902c419d6a7c404d57141ba56a690a9d83
dev_gyuil <a46884334@gmail.com>
2025-12-15 17:58:16 +0900

    2798

---
+++ 2798_블랙잭.py
@@ -0,0 +1,12 @@
+from itertools import combinations
+ 
+N, M = map(int, input().split())
+lst = list(map(int, input().split()))
+nlst = []
+ 
+for three in combinations(lst, 3):
+    if sum(three) > M:
+        continue
+    else:
+        nlst.append(sum(three))
+print(max(nlst))
