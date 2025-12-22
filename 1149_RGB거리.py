1dafd3addc7067f14f977f62162f05a199e9ec90
dev_gyuil <a46884334@gmail.com>
2025-12-22 19:00:48 +0900

    1149

--- 1149_RGB거리.py
+++ 1149_RGB거리.py
@@ -8,5 +8,6 @@ for i in range(1,n):
     a[i][0]= min(a[i-1][1],a[i-1][2]) + a[i][0]
     a[i][1]= min(a[i-1][0],a[i-1][2]) + a[i][1]
     a[i][2]= min(a[i-1][0],a[i-1][1]) + a[i][2]
+    
 
 print(min(a[n-1][0],a[n-1][1],a[n-1][2]))
