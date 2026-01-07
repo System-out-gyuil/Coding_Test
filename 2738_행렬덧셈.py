52800cecb0ff82afc5e41dea1f1d8bb86c86de68
dev_gyuil <a46884334@gmail.com>
2026-01-07 17:21:52 +0900

    2738

---
+++ 2738_행렬덧셈.py
@@ -0,0 +1,19 @@
+A, B = [], []
+
+N, M = map(int, input().split())
+
+for row in range(N):
+    row = list(map(int, input().split()))
+    A.append(row)
+
+for row in range(N):
+    row = list(map(int, input().split()))
+    B.append(row)
+
+print(A)
+print(B)
+
+for row in range(N):
+    for col in range(M):
+        print(A[row][col] + B[row][col], end=' ')
+    print()
